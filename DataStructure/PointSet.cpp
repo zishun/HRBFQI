55a067cfaf49520547cc4e154318df6eca2de760
Zishun Liu <liuzishun@gmail.com>
2019-06-21 10:58:09 +0200

    Update PointSet.cpp

--- DataStructure/PointSet.cpp
+++ DataStructure/PointSet.cpp
@@ -7,7 +7,7 @@
 #include <afxdisp.h>        // MFC automation classes
 #include <afxdtctl.h>		// MFC support for Internet Explorer common controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// support for Windows common controls.
+#include <afxcmn.h>			// MFC support for Windows common controls.
 #endif // _AFX_NO_AFXCMN_SUPPORT
 #else
 #include "stdio.h"
